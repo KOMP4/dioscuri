c103812b81cb02190701d4a522bd6d15afb4d0e0
KOMP4 <mishm.ru@gmail.com>
2024-05-28 09:39:16 +0300

    working...

--- utp data transfer/gps_pick.py
+++ utp data transfer/gps_pick.py
@@ -6,20 +6,20 @@ from time import sleep
 ###Settings
 UDP_IP = "0.0.0.0"
 UDP_PORT = 22222
-UART_PORT = '/dev/ttyUSB1'
+UART_PORT = 'COM7'
 
 #-----------------UDP initialisation---------------#
-sock = socket.socket(socket.AF_INET, # Internet
-                socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+#sock = socket.socket(socket.AF_INET, # Internet
+#                socket.SOCK_DGRAM) # UDP
+#sock.bind((UDP_IP, UDP_PORT))
 
 #-----------------UART initialisation--------------#
-ser = serial.Serial(
+gps = serial.Serial(
     port=UART_PORT,
     baudrate=115200,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
+    #parity=serial.PARITY_ODD,
+    #stopbits=serial.STOPBITS_TWO,
+    #bytesize=serial.SEVENBITS
 )
 sleep(2)#без этого уарт не робит!!!!
 
@@ -27,13 +27,13 @@ sleep(2)#без этого уарт не робит!!!!
 def informating(msg):
     msg = str(msg)
     print(msg)
-    sock.sendto(msg.encode(), (UDP_IP, UDP_PORT))
+    #sock.sendto(msg.encode(), (UDP_IP, UDP_PORT))
     
 
 def module_check(status):
     #пингуем модуль
-    ser.write(b"AT")
-    response1 = ser.readline().decode().strip()
+    gps.write(b"AT\n\r")
+    response1 = gps.readline().decode().strip()
     if response1 == "OK":
         informating("module ready...")
         status = 1
@@ -42,10 +42,10 @@ def module_check(status):
         status = 0
 def gps_on(status):
     #Включаем gps
-    ser.write(b"AT+CGPS=1")
+    gps.write(b"AT+CGPS=1\n\r")
     #Спрашиваем состояние
-    ser.write(b"AT+CGPS?")
-    response2 = ser.readline().decode().strip()
+    gps.write(b"AT+CGPS?\n\r")
+    response2 = gps.readline().decode().strip()
     if response2 == "+CGPS: 1,1":
         informating("gps ready")
         status = 1
@@ -55,13 +55,9 @@ def gps_on(status):
 
 
 def gps_info():
-    ser.write(b"AT+CGPSINFO")
-    return ser.readline().decode()
+    gps.write(b"AT+CGPSINFO\n\r")
+    return gps.readline().decode()
     ## не доделано
-
-
-
-
 
 status = 0
 module_check(status)
@@ -71,8 +67,9 @@ if status:
     switch = 1
     while switch:
         informating(f"location: {gps_info()}")
+        sleep(2)
 else:
     informating("error")
 
-sock.close()
-ser.close()
+#sock.close()
+gps.close()
